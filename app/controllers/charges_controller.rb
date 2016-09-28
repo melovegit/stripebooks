3d4eaba2baf7aeee2fe50324d97cfc2fa2360a85
paul <doublezeromedia@hotmail.com>
2016-09-28 14:26:03 +0000

    Initial commit

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,25 @@
+class ChargesController < ApplicationController
+  def create
+      @book = Book.find(params[:book_id])
+
+      # Amount is in cents
+      @amount = @book.price
+      @amount = @amount.to_i
+
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => @book.title,
+        :currency    => 'usd'
+      )
+      
+          rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+end
